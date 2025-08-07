9ef44b962cdd80a29c70c9ab367415874011461e
DarkEnough <DarkEnough@users.noreply.github.com>
2025-08-06 21:54:17 -0600

    Fix import

--- app/app.py
+++ app/app.py
@@ -11,6 +11,10 @@ import pandas as pd
 import streamlit as st
 import plotly.graph_objects as go
 
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from core_pipeline import (
     extract_text,
     Embedder,
